68733f64d313bb81cb18577d28360372f52ba972
Kavya Chheda <kavyachheda90@gmail.com>
2025-07-30 20:39:19 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,27 @@
+print(":)****************CALCULATOR*****************:)")
+
+
+a=input("ENTER THE FIRST NUMBER:")
+a=int(a)
+
+b=input("ENTER THE SECOND NUMBER:")
+b=int(b)
+print()
+print("Addition of the two number is:", a+b)
+print()
+print("Subtraction of two number is:", a-b)
+print()
+print("Multiplication of two number is: ", a*b)
+print()
+print("Division of two number is:",a/b)
+print()
+print("Modulus of two number is",a%b)
+print()
+print("Exponent of two number is:",a**b)
+print()
+print("Floor Division of two number is:",a//b)
+print()
+
+
+
+print("Thank you for using the calculator!")
